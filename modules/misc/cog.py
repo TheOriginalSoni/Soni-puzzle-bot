5968fc6a073d965d725a34d4e616c51ecc4601c0
Soni <the.original.soni@gmail.com>
2022-03-27 07:50:13 +0530

    If I.... open my commit to you

--- modules/misc/cog.py
+++ modules/misc/cog.py
@@ -28,12 +28,33 @@ class MiscCog(commands.Cog, name="Misc"):
         await ctx.send(embed=embed)
 
     @command_predicates.is_tester()
-    @commands.command(name="pranks", aliases=["puzzle1"])
+    @commands.command(name="part0", aliases=["puzzle0"])
+    async def part0(self, ctx):
+        logging_utils.log_command("part0", ctx.guild, ctx.channel, ctx.author)
+        embed = discord_utils.create_embed()
+        embed.add_field(
+            name=f"???",
+            value=f"Part 0 was finding the correct link on https://arithmancy.ueuo.com/ Since you're here, you already solved it. Right??????",
+            inline=False,
+        )
+        await ctx.send(embed=embed)
+
+    @command_predicates.is_tester()
+    @commands.command(name="part0ans", aliases=["pranks"])
+    async def part0ans(self, ctx):
+        logging_utils.log_command("part0ans", ctx.guild, ctx.channel, ctx.author)
+        embed = discord_utils.create_embed()
+        embed.add_field(
+            name=f"Correct!!",
+            value=f"Part 0 completed!",
+            inline=False,
+        )
+        await ctx.send(embed=embed)
+
+    @command_predicates.is_tester()
+    @commands.command(name="part1", aliases=["puzzle1"])
     async def pranks(self, ctx):
-        """
-        Usage : `~pranks`
-        """
-        logging_utils.log_command("pranks", ctx.guild, ctx.channel, ctx.author)
+        logging_utils.log_command("puzzle1", ctx.guild, ctx.channel, ctx.author)
         m1 = "<#030042000000100050060004040000005026000000000000000000000000000000000000000000000000000000000000000000000000000000000000>"
         m2 = "<#001100010100000001001100100000100001000000000000000000000000000000000000000000000000000000000000000000000000000000000000>"
         m3 = "<#683248722000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000>"
@@ -42,53 +63,98 @@ class MiscCog(commands.Cog, name="Misc"):
         await ctx.send(m3)
 
     @command_predicates.is_tester()
-    @commands.command(name="potato", aliases=["puzzle2"])
-    async def potato(self, ctx):
-        """
-        Usage : `~potato`
-        """
-        logging_utils.log_command("potato", ctx.guild, ctx.channel, ctx.author)
-        m1 = "https://cdn.discordapp.com/attachments/738306742139748422/955697060697501746/120.png"
+    @commands.command(name="part1ans", aliases=["potato"])
+    async def part1ans(self, ctx):
+        logging_utils.log_command("part1ans", ctx.guild, ctx.channel, ctx.author)
+        embed = discord_utils.create_embed()
+        embed.add_field(
+            name=f"Correct!!",
+            value=f"Part 1 completed!",
+            inline=False,
+        )
+        await ctx.send(embed=embed)
+
+    @command_predicates.is_tester()
+    @commands.command(name="part2", aliases=["puzzle2"])
+    async def part2(self, ctx):
+        logging_utils.log_command("part2", ctx.guild, ctx.channel, ctx.author)
+        m1 = "https://cdn.discordapp.com/attachments/824774417649106955/957458640523116584/120.png"
         await ctx.send(m1)
 
     @command_predicates.is_tester()
-    @commands.command(name="pariah", aliases=["puzzle3"])
-    async def pariah(self, ctx):
-        """
-        Usage : `~pariah`
-        """
-        logging_utils.log_command("pariah", ctx.guild, ctx.channel, ctx.author)
-        loc = "files/sounding.zip"
-        file = nextcord.File(loc)
-        await ctx.send(file=file)
+    @commands.command(name="part2ans", aliases=["pariah"])
+    async def part2ans(self, ctx):
+        logging_utils.log_command("part2ans", ctx.guild, ctx.channel, ctx.author)
+        embed = discord_utils.create_embed()
+        embed.add_field(
+            name=f"Correct!!",
+            value=f"Part 2 completed!",
+            inline=False,
+        )
+        await ctx.send(embed=embed)
 
     @command_predicates.is_tester()
-    @commands.command(name="poetry", aliases=["puzzle4"])
-    async def poetry(self, ctx):
-        """
-        Usage : `~poetry`
-        """
-        logging_utils.log_command("poetry", ctx.guild, ctx.channel, ctx.author)
+    @commands.command(name="part3", aliases=["puzzle3"])
+    async def puzzle3(self, ctx):
+        logging_utils.log_command("puzzle3", ctx.guild, ctx.channel, ctx.author)
         m1 = "__On__e __star__ in __every__ __same__ one __?__ Ba__ttle__ __who__ ?Add __sect__ors __upon__ __it.__"
         await ctx.send(m1)
 
     @command_predicates.is_tester()
-    @commands.command(name="polska", aliases=["puzzle5"])
-    async def polska(self, ctx):
-        """
-        Usage : `~polska`
-        """
-        logging_utils.log_command("polska", ctx.guild, ctx.channel, ctx.author)
+    @commands.command(name="part3ans", aliases=["poetry"])
+    async def part3ans(self, ctx):
+        logging_utils.log_command("part3ans", ctx.guild, ctx.channel, ctx.author)
+        embed = discord_utils.create_embed()
+        embed.add_field(
+            name=f"Correct!!",
+            value=f"Part 3 completed!",
+            inline=False,
+        )
+        await ctx.send(embed=embed)
+
+    @command_predicates.is_tester()
+    @commands.command(name="part4", aliases=["puzzle4"])
+    async def part4(self, ctx):
+        logging_utils.log_command("part4", ctx.guild, ctx.channel, ctx.author)
+        loc = "files/phone.zip"
+        file = nextcord.File(loc)
+        await ctx.send(file=file)
+
+    @command_predicates.is_tester()
+    @commands.command(name="part4ans", aliases=["polska"])
+    async def part4ans(self, ctx):
+        logging_utils.log_command("part4ans", ctx.guild, ctx.channel, ctx.author)
+        embed = discord_utils.create_embed()
+        embed.add_field(
+            name=f"Correct!!",
+            value=f"Part 4 completed!",
+            inline=False,
+        )
+        await ctx.send(embed=embed)
+
+    @command_predicates.is_tester()
+    @commands.command(name="part5", aliases=["puzzle5"])
+    async def part5(self, ctx):
+        logging_utils.log_command("part5", ctx.guild, ctx.channel, ctx.author)
         m1 = "https://cdn.discordapp.com/attachments/738306742139748422/955691396109565952/irrational.png"
         await ctx.send(m1)
 
     @command_predicates.is_tester()
-    @commands.command(name="pirate", aliases=["puzzle6"])
-    async def pirate(self, ctx):
-        """
-        Usage : `~pirate`
-        """
-        logging_utils.log_command("pirate", ctx.guild, ctx.channel, ctx.author)
+    @commands.command(name="part5ans", aliases=["pirate"])
+    async def part5ans(self, ctx):
+        logging_utils.log_command("part5ans", ctx.guild, ctx.channel, ctx.author)
+        embed = discord_utils.create_embed()
+        embed.add_field(
+            name=f"Correct!!",
+            value=f"Part 5 completed!",
+            inline=False,
+        )
+        await ctx.send(embed=embed)
+
+    @command_predicates.is_tester()
+    @commands.command(name="part6", aliases=["puzzle6"])
+    async def part6(self, ctx):
+        logging_utils.log_command("part6", ctx.guild, ctx.channel, ctx.author)
         m1 = "**Including itself, what is the quickest way out of this hellscape?**\n\n Note - This is the last puzzle and goes into submission box. \n\n Also while we're here, what's your favourite thing about Arithmancy?"
         await ctx.send(m1)
 
